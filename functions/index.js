f247a17bca1bc52ed8502d92e67f52bda8f283dc
Kyohei Tsuda <shimano.entou@gmail.com>
2017-07-29 10:28:24 +0900

    Add API.AI's actions.

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,15 @@ process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 
 //API.AI actions
-const SAY_HELLO = 'say.hello';
+const TV_ON = 'tv.on';
+const AC_ON = 'ac.on';
+const LIGHT_ON = 'light.on';
+const CAT_LIGHT_ON = 'catlight.on';
+
+const TV_OFF = 'tv.off';
+const AC_OFF = 'ac.off';
+const LIGHT_OFF = 'light.off';
+const CAT_LIGHT_OFF = 'catlight.off';
 
 exports.IRKit = (request, response) => {
   const app = new App({request, response});
@@ -18,14 +26,62 @@ exports.IRKit = (request, response) => {
   console.log('Request body: ' + JSON.stringify(request.body));
 
   // Fulfill action business logic
-  function helloHandler (app) {
-    // Complete your fulfillment logic and send a response
-    console.log('Requested ' + SAY_HELLO);
-    app.ask('Hello!!');
+  function tv_on_handler (app) {
+    console.log('Requested ' + TV_ON);
+    app.ask('Your requested turn on tv');
+    app.ask('Okay here we go.');
+  }
+
+  function ac_on_handler (app) {
+    console.log('Requested ' + AC_ON);
+    app.ask('Your requested turn on AC');
+    app.ask('Okay here we go.');
+  }
+
+  function light_on_handler (app) {
+    console.log('Requested ' + LIGHT_ON);
+    app.ask('Your requested turn on light');
+    app.ask('Okay here we go.');
+  }
+
+  function cat_light_on_handler (app) {
+    console.log('Requested ' + CAT_LIGHT_ON);
+    app.ask('Your requested turn on cat light');
+    app.ask('Okay here we go.');
+  }
+
+  function tv_off_handler (app) {
+    console.log('Requested ' + TV_OFF);
+    app.ask('Your requested turn off tv');
+    app.ask('Okay here we go.');
+  }
+
+  function ac_off_handler (app) {
+    console.log('Requested ' + AC_OFF);
+    app.ask('Your requested turn off AC');
+    app.ask('Okay here we go.');
+  }
+
+  function light_off_handler (app) {
+    console.log('Requested ' + LIGHT_OFF);
+    app.ask('Your requested turn off light');
+    app.ask('Okay here we go.');
+  }
+
+  function cat_light_off_handler (app) {
+    console.log('Requested ' + CAT_LIGHT_OFF);
+    app.ask('Your requested turn off cat light');
+    app.ask('Okay here we go.');
   }
 
   const actionMap = new Map();
-  actionMap.set(SAY_HELLO, helloHandler);
-
+  actionMap.set(TV_ON, tv_on_handler);
+  actionMap.set(AC_ON, ac_on_handler);
+  actionMap.set(LIGHT_ON, light_on_handler);
+  actionMap.set(CAT_LIGHT_ON, cat_light_on_handler);
+  actionMap.set(TV_OFF, tv_off_handler);
+  actionMap.set(AC_OFF, ac_off_handler);
+  actionMap.set(LIGHT_OFF, light_off_handler);
+  actionMap.set(CAT_LIGHT_OFF, cat_light_off_handler);
   app.handleRequest(actionMap);
 };
